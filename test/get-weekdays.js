6c8d5b26934a6b872710c51ec721ba623783b97a
dimaslanjaka <dimaslanjaka@gmail.com>
2025-05-09 22:41:05 +0700

    feat(test): add random weekday selection to test output

--- test/get-weekdays.js
+++ test/get-weekdays.js
@@ -1,3 +1,5 @@
+import { array_random } from 'sbg-utility';
 import { getWeekdaysOfCurrentMonth } from '../src/utils.js';
 
 console.log(getWeekdaysOfCurrentMonth(true));
+console.log('random:', array_random(getWeekdaysOfCurrentMonth()));
