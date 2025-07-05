e814c719be294c4c5ac3dd2817383629dd2aee8b
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-05 15:36:11 +0700

    feat: add delay to nodemon execution for improved performance

--- bin/ndm.cmd
+++ bin/ndm.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-npx nodemon --watch src --watch %1 --exec "node %*" --ext js --ignore .gitignore --ignore .vscode --ignore node_modules --ignore package-lock.json --ignore package.json --ignore README.md --ignore LICENSE
+npx nodemon --delay 5 --watch src --watch %1 --exec "node %*" --ext js --ignore .gitignore --ignore .vscode --ignore node_modules --ignore package-lock.json --ignore package.json --ignore README.md --ignore LICENSE
