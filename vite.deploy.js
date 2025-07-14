5ee02c9a8b9947f4d4a0d953f7f81976145674aa
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-15 05:47:54 +0700

    feat: add vite:deploy script and update dependencies for gh-pages

---
+++ vite.deploy.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import ghpages from 'gh-pages';
+
+function callback(err) {
+  if (err) {
+    console.error('Error during deployment:', err);
+  } else {
+    console.log('Deployment successful!');
+  }
+}
+ghpages.publish(
+  'dist',
+  {
+    nojekyll: true,
+    repo: 'https://dimaslanjaka:' + process.env.ACCESS_TOKEN + '@github.com/dimaslanjaka/browser-automation.git',
+    branch: 'gh-pages',
+    user: { name: 'dimaslanjaka', email: 'dimaslanjaka@gmail.com' },
+    message: `Deploying to GitHub Pages: ${new Date().toISOString()}`
+  },
+  callback
+);
