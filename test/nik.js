b81000003daa133a5ccd7395f9289ffa93706557
dimaslanjaka <dimaslanjaka@gmail.com>
2025-07-16 12:53:39 +0700

    fix: add additional NIK for testing and update selection index

--- test/nik.js
+++ test/nik.js
@@ -1,8 +1,8 @@
 import region_controller from 'nik-parser-jurusid';
 
-const niks = ['4360000000000658', '3678101007220001'];
+const niks = ['4360000000000658', '3678101007220001', '3578104704210002'];
 
-const nik = niks[0]; // Change this to test different NIKs
+const nik = niks[2]; // Change this to test different NIKs
 const regionResult = region_controller(nik);
 
 console.log({
