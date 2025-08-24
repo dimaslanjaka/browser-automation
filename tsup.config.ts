8576c30454cba54af2ee8947db6d141be5418f8e
dimaslanjaka <dimaslanjaka@gmail.com>
2025-08-25 05:43:40 +0700

    feat(tsup.config): enable code splitting and treeshaking for improved bundle optimization

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,10 +4,12 @@ export default defineConfig({
   entry: ['src/**/*.js', 'src/**/*.cjs', 'src/**/*.mjs', 'src/**/*.ts'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
   dts: false,
+  treeshake: true,
+  bundle: false,
   minify: false,
   target: 'node16',
   shims: false
