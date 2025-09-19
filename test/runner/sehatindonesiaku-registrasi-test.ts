6e85c109c6580e55ecb91bc384986cdd534773fd
dimaslanjaka <dimaslanjaka@gmail.com>
2025-09-19 13:42:47 +0700

    feat: include database parameter in processRegistrasiData function

--- test/runner/sehatindonesiaku-registrasi-test.ts
+++ test/runner/sehatindonesiaku-registrasi-test.ts
@@ -1,9 +1,10 @@
 import { getPuppeteer } from '../../src/puppeteer_utils.js';
-import { getExcelData } from '../../src/runner/sehatindonesiaku-data.js';
+import { getExcelData, getSehatIndonesiaKuDb } from '../../src/runner/sehatindonesiaku-data.js';
 import { processRegistrasiData } from '../../src/runner/sehatindonesiaku-registrasi.js';
 
 async function main() {
-  const data = (await getExcelData()).find((item) => item.nik === '3174077110620001');
+  const db = await getSehatIndonesiaKuDb();
+  const data = (await getExcelData()).find((item) => item.nik === '3173055506680016');
   if (!data) {
     throw new Error('Data not found');
   }
@@ -11,7 +12,7 @@ async function main() {
 
   try {
     console.log(`Process`, data);
-    await processRegistrasiData(page, data);
+    await processRegistrasiData(page, data, db);
   } catch (error) {
     console.error('Error processing registrasi data:', error);
   }
