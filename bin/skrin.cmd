bd0174347ca4d21813d2510d7cfb1f2c4e12b3bb
dimaslanjaka <dimaslanjaka@gmail.com>
2026-02-25 19:42:15 +0700

    fix(skrin): add process and database build options to skrin.cmd

--- bin/skrin.cmd
+++ bin/skrin.cmd
@@ -13,6 +13,20 @@ if /I "%~1"=="build" (
 	exit /b %ERRORLEVEL%
 )
 
+if /I "%~1"=="process" (
+  echo building process...
+  set "BUILD_TARGET=process"
+  call rollup -c rollup.skrin.js
+  exit /b %ERRORLEVEL%
+)
+
+if /I "%~1"=="database" (
+  echo building database...
+  set "BUILD_TARGET=database"
+  call rollup -c rollup.skrin.js
+  exit /b %ERRORLEVEL%
+)
+
 @REM set "SKRIN_INPUT=%CD%\src\runner\skrin2.js"
 set "SKRIN_INPUT=%CD%\src\runner\skrin.direct.js"
 @REM set "SKRIN_INPUT=%CD%\src\runner\skrin-cluster.ts"
