b931098bd73fdec2121b58f4de05b36be18a6430
dimaslanjaka <dimaslanjaka@gmail.com>
2026-06-16 00:07:36 +0700

    docs(build): add EndpointManager to package exports for lazy loading

--- src/index-exports.ts
+++ src/index-exports.ts
@@ -4,5 +4,7 @@ import '../.vscode/js-hook.cjs';
 // Re-export all modules
 
 export * from './utils/index.js';
-export * from './puppeteer/index.js';
 export * from './database/index.js';
+
+// Export EndpointManager (puppeteer utility, loaded lazily on access)
+export { EndpointManager, GLOBAL_ENDPOINT_MANAGER_PATH, GLOBAL_PUPPETEER_DIR } from './puppeteer/parallel/EndpointManager.js';
