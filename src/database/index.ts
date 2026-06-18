766d0eb7d65eba69d1ef3749d99dc527be952f61
dimaslanjaka <dimaslanjaka@gmail.com>
2026-06-19 04:17:46 +0700

    feat(db): export MySQLHelper
    index.ts → export MySQLHelper → pub API access.

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,3 +2,4 @@ export { LogDatabase, LogDatabaseOptions } from './LogDatabase.js';
 export { MysqlLogDatabase, AddLogOptions, defaultOptions as mysqlDefaultOptions } from './MysqlLogDatabase.js';
 export { SQLiteLogDatabase, getDatabaseFilePath } from './SQLiteLogDatabase.js';
 export * from './db_utils.js';
+export * from './MySQLHelper.js';
